b620c74dbd7a4e6a35dd026e222a02318f5e9767
LewisDamy <ldamy16@gmail.com>
2022-12-20 16:15:55 -0300

    added url azure to allow hosts

--- myWebSite/settings.py
+++ myWebSite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://personal-website-lewis.azurewebsites.net/"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://personal-website-lewis.azurewebsites.net"]
 
 
 # Application definition
